fed9615af3d6d3a4ba364074e67ac905a7d9b057
mig <mig@pop-os.localdomain>
2022-08-25 22:09:10 -0300

    Hello, World

---
+++ desconto.py
@@ -0,0 +1,51 @@
+#=== FUNÇÃO PARA ADICIONAR BORDAS AO TEXTO ===#
+
+def borda(texto,tamanho):
+  print('\n+','-'*tamanho,'+')
+  print('|',texto,'|')
+  print('+','-'*tamanho,'+')
+
+# Texto
+
+txt = 'Bem vindo(a) a loja do Ivalber Miguel'
+tam = len(txt)
+borda(txt,tam)
+
+#=== ENTRADAS ===#
+
+valor = float(input('Digite o valor do produto: ')) 
+quant = float(input('|\nDigite a quantidade do produto: '))
+
+# Condicionais
+
+if(quant >= 100):
+  desc = 10.0
+  print('|\nValor sem desconto: {}R$'.format(valor*quant))
+  valor = valor * quant
+  desc = (desc * valor)/100
+  valor = valor - desc
+  print('Valor com desconto: {}R$ (desconto de 10%)\n'.format(valor))
+
+elif(quant >= 20):
+  desc = 6.0
+  print('|\nValor sem desconto: {}R$'.format(valor*quant))
+  valor = valor * quant
+  desc = (desc * valor)/100
+  valor = valor - desc
+  print('Valor com desconto: {}R$ (desconto de 6%)\n'.format(valor))
+
+elif(quant > 4):
+  desc = 3.0
+  print('|\nValor sem desconto: {}R$'.format(valor*quant))
+  valor = valor * quant
+  desc = (desc * valor)/100
+  valor = valor - desc
+  print('Valor com desconto: {}R$ (desconto de 3%)\n'.format(valor))
+
+else:
+  desc = 0
+  print('|\nValor sem desconto: {}R$'.format(valor*quant))
+  valor = valor * quant
+  desc = (desc * valor)/100
+  valor = valor - desc
+  print('Valor com desconto: {}R$ (desconto de 0%)\n'.format(valor))
